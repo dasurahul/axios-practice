ab59b80ff65d46cdd526e13eb7003a909fe89962
dasurahul <dasurahul598@gmail.com>
2021-06-10 21:31:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import Container from "react-bootstrap/Container";
+import ListGroup from "react-bootstrap/ListGroup";
+const App = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        setPosts(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-function App() {
+  if (loading) {
+    return (
+      <Container>
+        <h1>Loading</h1>
+      </Container>
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-primary text-center mt-4 mb-4">Posts</h1>
+      <Container>
+        <ListGroup>
+          {posts.map((post) => {
+            return (
+              <ListGroup.Item className="mt-2 mb-2" key={post.id}>
+                {post.body}
+              </ListGroup.Item>
+            );
+          })}
+        </ListGroup>
+      </Container>
     </div>
   );
-}
+};
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+describe("App Component by me", () => {
+  test("renders posts heading", () => {
+    render(<App />);
+    const linkElement = screen.findByText(/posts/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
